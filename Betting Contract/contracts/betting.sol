49d73cb8b11b96b52931a6146bec428e9e6eda35
Felipe Rocha Campos <fmrcampos@berkeley.edu>
2018-02-23 11:21:39 -0800

    more work on betting contract

--- Betting Contract/contracts/betting.sol
+++ Betting Contract/contracts/betting.sol
@@ -67,10 +67,15 @@ contract Betting {
 
     /* Gamblers place their bets, preferably after calling checkOutcomes */
     function makeBet(uint _outcome) public payable returns (bool) {
+      if (oracle == null) {
+        revert('')
+      }
+
       if (gamblerA == null) {
         gamblerA = msg.sender;
       } else if (gamblerB == null) {
         gamblerB = msg.sender;
+        BetClosed();
       } else {
         revert('There are already two gamblers, please try again once winnings are dispersed.');
         return false;
@@ -84,26 +89,52 @@ contract Betting {
 
     /* The oracle chooses which outcome wins */
     function makeDecision(uint _outcome) public oracleOnly() outcomeExists(_outcome) {
-      if bets[gamblerA]
+      uint pot = bets[gamblerA].amount + bets[gamblerB].amount;
+      if ((bets[gamblerA] == _outcome) && (bets[gamblerB] == _outcome){
+        winnings[gamblerA] = pot / 2.0;
+        winnings[gamblerB] = pot / 2.0;
+      } else if (bets[gamblerA] == _outcome) {
+        winnings[gamblerA] = pot;
+      } else if (bets[gamblerB] == _outcome) {
+        winnings[gamblerB] = pot;
+      }
+
+      gamblerA = null;
+      gamblerB = null;
     }
 
     /* Allow anyone to withdraw their winnings safely (if they have enough) */
     function withdraw(uint withdrawAmount) public returns (uint) {
-      //
+      uint cashMoneyFlow = winnings[msg.sender];
+      if (withdrawAmount <= cashMoneyFlow) {
+        winnings[msg.sender] -= withdrawAmount;
+        if (!msg.sender.send(withdrawAmount)) {
+          winnings[msg.sender] += withdrawAmount;
+        }
+      }
+
+      return winnings[msg.sender];
     }
 
     /* Allow anyone to check the outcomes they can bet on */
     function checkOutcomes(uint outcome) public view returns (uint) {
-      //
+      for (int i = 0; i < outcomesCount; i += 1) {
+        if (outcome[i] == outcome) {
+          return 1;
+        }
+      }
+      return 0;
     }
 
     /* Allow anyone to check if they won any bets */
     function checkWinnings() public view returns(uint) {
-      //
+      return winnings[msg.sender];
     }
 
     /* Call delete() to reset certain state variables. Which ones? That's upto you to decide */
     function contractReset() public ownerOnly() {
-      //
+      oracle.delete();
+      gamblerA.delete();
+      gamblerB.delete();
     }
 }
